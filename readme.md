33d59a43eb5e6579258cdb67aa448787b7b50cee
Nick Golebiewski <nick.golebiewski@gmail.com>
2024-07-10 10:19:37 -0400

    first commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Socket Sound Machine
+
+Experiment to learn Socket.io and implement a multi-player sound machine/instrument.
+
---
+++ src/server/main.js
@@ -0,0 +1,14 @@
+import express from "express";
+import ViteExpress from "vite-express";
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/hello", (req, res) => {
+  res.send("Hello Vite + React!");
+});
+
+ViteExpress.listen(app, PORT, () =>
+  console.log(`Server is listening on port ${PORT}...`),
+);
